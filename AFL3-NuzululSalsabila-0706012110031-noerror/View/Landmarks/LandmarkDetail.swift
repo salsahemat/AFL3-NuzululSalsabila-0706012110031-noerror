6790c1f26aa28fa275a8240a92b3fcccb2c0ba2e
Nuzulul-Salsabila <nsalsabila02@student.ciputra.ac.id>
2023-04-28 01:38:40 +0700

    section 2 chapter : Creating MacOS App forgot

--- AFL3-NuzululSalsabila-0706012110031-noerror/View/Landmarks/LandmarkDetail.swift
+++ AFL3-NuzululSalsabila-0706012110031-noerror/View/Landmarks/LandmarkDetail.swift
@@ -6,6 +6,7 @@
 //
 
 import SwiftUI
+import MapKit
 
 //define landmarkdetail to show detail in landmark
 struct LandmarkDetail: View {
@@ -19,34 +20,45 @@ struct LandmarkDetail: View {
     
     var body: some View {
         ScrollView {
-            MapView(coordinate: landmark.locationCoordinate)
-            //to extend to the top edge of the screen
-                .ignoresSafeArea(edges: .top)
-                .frame(height: 300)
-            
-            CircleImage(image: landmark.image)
-            // moving the image upwards
-                .offset(y: -130)
-                .padding(.bottom, -130)
-            
-            VStack(alignment: .leading) {
-                HStack {
-                    Text(landmark.name)
-                        .font(.title)
-                    FavoriteButton(isSet: $modelData.landmarks[landmarkIndex].isFavorite)
+            ZStack(alignment: Alignment(horizontal: .trailing, vertical: .top)) {
+                MapView(coordinate: landmark.locationCoordinate)
+                //to extend to the top edge of the screen
+                    .ignoresSafeArea(edges: .top)
+                    .frame(height: 300)
+                
+                Button("Open in Maps") {
+                    let destination = MKMapItem(placemark: MKPlacemark(coordinate: landmark.locationCoordinate))
+                    destination.name = landmark.name
+                    destination.openInMaps()
                 }
-                
-                Text(landmark.name)
-                    .font(.title)
-                
-                HStack {
-                    Text(landmark.park)
-                    Spacer()
-                    Text(landmark.state)
+                .padding()
+            }
+            
+            VStack(alignment: .leading, spacing: 20) {
+                HStack(spacing: 24) {
+                    CircleImage(image: landmark.image.resizable())
+                        .frame(width: 160, height: 160)
+                    
+                    VStack(alignment: .leading) {
+                        HStack {
+                            Text(landmark.name)
+                                .font(.title)
+                            FavoriteButton(isSet: $modelData.landmarks[landmarkIndex].isFavorite)
+                                .buttonStyle(.plain)
+                        }
+                        
+                        Text(landmark.name)
+                            .font(.title)
+                        
+                        VStack(alignment: .leading) {
+                            Text(landmark.park)
+                            Spacer()
+                            Text(landmark.state)
+                        }
+                        .font(.subheadline)
+                        .foregroundColor(.secondary)
+                    }
                 }
-                .font(.subheadline)
-                .foregroundColor(.secondary)
-                
                 Divider()
                 
                 Text("About \(landmark.name)")
@@ -54,6 +66,8 @@ struct LandmarkDetail: View {
                 Text(landmark.description)
             }
             .padding()
+            .frame(maxWidth: 700)
+            .offset(y: -50)
             
         }
         //give the navigation bar a title
@@ -70,5 +84,6 @@ struct LandmarkDetail_Previews: PreviewProvider {
     static var previews: some View {
         LandmarkDetail(landmark: modelData.landmarks[0])
             .environmentObject(modelData)
+            .frame(width: 850, height: 700)
     }
 }
