659d06c5df9a61f56ef569bc414cc52f9455638c
Nuzulul-Salsabila <nsalsabila02@student.ciputra.ac.id>
2023-04-27 21:19:26 +0700

    section 5

---
+++ AFL3-NuzululSalsabila-0706012110031-noerror/MapView.swift
@@ -0,0 +1,28 @@
+//
+//  MapView.swift
+//  AFL3-NuzululSalsabila-0706012110031-noerror
+//
+//  Created by Nuzulul Salsabila on 27/04/23.
+//
+
+import SwiftUI
+import MapKit
+
+struct MapView: View {
+    //holds the region information for the map
+    @State private var region = MKCoordinateRegion(
+        center: CLLocationCoordinate2D(latitude: 34.011_286, longitude: -116.166_868),
+        span: MKCoordinateSpan(latitudeDelta: 0.2, longitudeDelta: 0.2)
+    )
+    
+    //takes a binding to the region
+    var body: some View {
+        Map(coordinateRegion: $region)
+    }
+}
+
+struct MapView_Previews: PreviewProvider {
+    static var previews: some View {
+        MapView()
+    }
+}
