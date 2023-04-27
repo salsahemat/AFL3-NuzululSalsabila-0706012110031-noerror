66e4cf9e8bc2af00602d9ddb31015067cccb3c1a
Nuzulul-Salsabila <nsalsabila02@student.ciputra.ac.id>
2023-04-27 23:03:41 +0700

    section 1 chapter 3: Composing complex interfaces

---
+++ AFL3-NuzululSalsabila-0706012110031-noerror/View/Categories/CategoryHome.swift
@@ -0,0 +1,23 @@
+//
+//  CategoryHome.swift
+//  AFL3-NuzululSalsabila-0706012110031-noerror
+//
+//  Created by Nuzulul Salsabila on 27/04/23.
+//
+
+import SwiftUI
+
+struct CategoryHome: View {
+    var body: some View {
+        NavigationView {
+            Text("Hello, World!")
+                .navigationTitle("Featured")
+        }
+    }
+}
+
+struct CategoryHome_Previews: PreviewProvider {
+    static var previews: some View {
+        CategoryHome()
+    }
+}
