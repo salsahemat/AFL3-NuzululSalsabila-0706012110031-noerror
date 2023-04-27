e91fa680b44c2db3986bb0b4368ff5bb26579c5b
Nuzulul-Salsabila <nsalsabila02@student.ciputra.ac.id>
2023-04-27 17:47:47 +0700

    Initial Commit

---
+++ AFL3-NuzululSalsabila-0706012110031-noerror/AFL3_NuzululSalsabila_0706012110031_noerrorApp.swift
@@ -0,0 +1,17 @@
+//
+//  AFL3_NuzululSalsabila_0706012110031_noerrorApp.swift
+//  AFL3-NuzululSalsabila-0706012110031-noerror
+//
+//  Created by Nuzulul Salsabila on 27/04/23.
+//
+
+import SwiftUI
+
+@main
+struct AFL3_NuzululSalsabila_0706012110031_noerrorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
