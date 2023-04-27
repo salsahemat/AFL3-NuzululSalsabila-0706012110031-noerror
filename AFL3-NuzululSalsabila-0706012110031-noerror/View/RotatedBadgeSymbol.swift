77e7e971b81b04dd954c0b3a38677ea395f922b0
Nuzulul-Salsabila <nsalsabila02@student.ciputra.ac.id>
2023-04-27 22:34:24 +0700

    section 3

---
+++ AFL3-NuzululSalsabila-0706012110031-noerror/View/RotatedBadgeSymbol.swift
@@ -0,0 +1,26 @@
+//
+//  RotatedBadgeSymbol.swift
+//  AFL3-NuzululSalsabila-0706012110031-noerror
+//
+//  Created by Nuzulul Salsabila on 27/04/23.
+//
+
+import SwiftUI
+
+//encapsulate the concept of a rotated symbol
+struct RotatedBadgeSymbol: View {
+    let angle: Angle
+    
+    var body: some View {
+        BadgeSymbol()
+            .padding(-60)
+            .rotationEffect(angle, anchor: .bottom)
+    }
+}
+
+struct RotatedBadgeSymbol_Previews: PreviewProvider {
+    static var previews: some View {
+        //to encapsulate the concept of a rotated symbol
+        RotatedBadgeSymbol(angle: Angle(degrees: 5))
+    }
+}
