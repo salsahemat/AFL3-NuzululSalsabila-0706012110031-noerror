008fc4e6789d2ede7733f917576dc2a673f46408
Nuzulul-Salsabila <nsalsabila02@student.ciputra.ac.id>
2023-04-28 01:02:57 +0700

    section 4

--- AFL3-NuzululSalsabila-0706012110031-noerror/View/Landmarks/LandmarkList.swift
+++ AFL3-NuzululSalsabila-0706012110031-noerror/View/Landmarks/LandmarkList.swift
@@ -11,12 +11,29 @@ struct LandmarkList: View {
     @EnvironmentObject var modelData: ModelData
     //called  showFavoritesOnly set to false to see how the list reacts
     @State private var showFavoritesOnly = false
+    @State private var filter = FilterCategory.all
+    
+    //describe filter states
+    enum FilterCategory: String, CaseIterable, Identifiable {
+        case all = "All"
+        case lakes = "Lakes"
+        case rivers = "Rivers"
+        case mountains = "Mountains"
+        
+        var id: FilterCategory { self }
+    }
     
     //checking the showFavoritesOnly property and each landmark
     var filteredLandmarks: [Landmark] {
         modelData.landmarks.filter { landmark in
             (!showFavoritesOnly || landmark.isFavorite)
+            && (filter == .all || filter.rawValue == landmark.category.rawValue)
         }
+    }
+    
+    var title: String {
+        let title = filter == .all ? "Landmarks" : filter.rawValue
+        return showFavoritesOnly ? "Favorite \(title)" : title
     }
     
     
@@ -24,11 +41,6 @@ struct LandmarkList: View {
         NavigationView {
             //creates one LandmarkRow for each element in the landmarks array
             List {
-                //passing a binding to showFavoritesOnly
-                Toggle(isOn: $showFavoritesOnly) {
-                    Text("Favorites only")
-                }
-                
                 ForEach(filteredLandmarks) { landmark in
                     NavigationLink {
                         LandmarkDetail(landmark: landmark)
@@ -37,9 +49,31 @@ struct LandmarkList: View {
                     }
                 }
             }
-            .navigationTitle("Landmarks")
+            .navigationTitle(title)
             //ensures that the list never becomes too small
             .frame(minWidth: 300)
+            //containing a Menu inside a new toolbar modifier
+            .toolbar {
+                ToolbarItem {
+                    Menu {
+                        Picker("Category", selection: $filter) {
+                            ForEach(FilterCategory.allCases) { category in
+                                Text(category.rawValue).tag(category)
+                            }
+                        }
+                        .pickerStyle(.inline)
+                        
+                        //passing a binding to showFavoritesOnly
+                        Toggle(isOn: $showFavoritesOnly) {
+                            Label("Favorites only", systemImage: "star.fill")
+                        }
+                    } label: {
+                        Label("Filter", systemImage: "slider.horizontal.3")
+                    }
+                }
+            }
+            //placeholder for the second view in wide layouts
+            Text("Select a Landmark")
         }
     }
 }
