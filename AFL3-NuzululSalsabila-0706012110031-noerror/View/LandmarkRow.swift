63f8754300d25696a1480d3ea93de140f6580881
Nuzulul-Salsabila <nsalsabila02@student.ciputra.ac.id>
2023-04-27 21:36:26 +0700

    section 2

---
+++ AFL3-NuzululSalsabila-0706012110031-noerror/View/LandmarkRow.swift
@@ -0,0 +1,30 @@
+//
+//  LandmarkRow.swift
+//  AFL3-NuzululSalsabila-0706012110031-noerror
+//
+//  Created by Nuzulul Salsabila on 27/04/23.
+//
+
+import SwiftUI
+
+struct LandmarkRow: View {
+    var landmark: Landmark
+    
+    var body: some View {
+        HStack {
+            landmark.image
+                .resizable()
+                .frame(width: 50, height: 50)
+            Text(landmark.name)
+            
+            Spacer()
+        }
+    }
+}
+
+struct LandmarkRow_Previews: PreviewProvider {
+    static var previews: some View {
+        //specifying the first element of the landmarks array
+        LandmarkRow(landmark: landmarks[0])
+    }
+}
