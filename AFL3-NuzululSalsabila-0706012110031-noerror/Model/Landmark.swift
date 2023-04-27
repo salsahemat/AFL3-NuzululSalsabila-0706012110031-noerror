adfa848678fa5585834b66098338c5c66809936a
Nuzulul-Salsabila <nsalsabila02@student.ciputra.ac.id>
2023-04-27 21:33:53 +0700

    Section 1 Chapter 1 : Building list and navigation

---
+++ AFL3-NuzululSalsabila-0706012110031-noerror/Model/Landmark.swift
@@ -0,0 +1,38 @@
+//
+//  Landmark.swift
+//  AFL3-NuzululSalsabila-0706012110031-noerror
+//
+//  Created by Nuzulul Salsabila on 27/04/23.
+//
+
+import Foundation
+import SwiftUI
+import CoreLocation
+
+//define landmark structure
+struct Landmark: Hashable, Codable {
+    var id: Int
+    var name: String
+    var park: String
+    var state: String
+    var description: String
+    
+    //read and computed the name of the image from the data
+    private var imageName: String
+    var image: Image {
+        Image(imageName)
+    }
+    
+    private var coordinates: Coordinates
+    var locationCoordinate: CLLocationCoordinate2D {
+        CLLocationCoordinate2D(
+            latitude: coordinates.latitude,
+            longitude: coordinates.longitude)
+    }
+    
+    struct Coordinates: Hashable, Codable {
+        var latitude: Double
+        var longitude: Double
+    }
+    
+}
