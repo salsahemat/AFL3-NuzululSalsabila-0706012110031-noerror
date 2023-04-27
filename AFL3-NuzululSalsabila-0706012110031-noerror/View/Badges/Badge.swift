a231687d508c32a61f3b1ea192db7d1a45f15ea0
Nuzulul-Salsabila <nsalsabila02@student.ciputra.ac.id>
2023-04-27 22:40:38 +0700

    section 4, chapter 2:Drawing Paths and Shapes done!

---
+++ AFL3-NuzululSalsabila-0706012110031-noerror/View/Badges/Badge.swift
@@ -0,0 +1,41 @@
+//
+//  Badge.swift
+//  AFL3-NuzululSalsabila-0706012110031-noerror
+//
+//  Created by Nuzulul Salsabila on 27/04/23.
+//
+
+import SwiftUI
+
+struct Badge: View {
+    //to rotate and display copies of the badge symbol
+    var badgeSymbols: some View {
+        //creates a sun-like pattern by repeating the mountain symbol
+        ForEach(0..<8) { index in
+            RotatedBadgeSymbol(
+                angle: .degrees(Double(index) / Double(8)) * 360.0
+            )
+        }
+        .opacity(0.5)
+    }
+    
+    var body: some View {
+        ZStack {
+            BadgeBackground()
+            
+            //reading the surrounding geometry and scaling the symbol
+            GeometryReader { geometry in
+                badgeSymbols
+                    .scaleEffect(1.0 / 4.0, anchor: .top)
+                    .position(x: geometry.size.width / 2.0, y: (3.0 / 4.0) * geometry.size.height)
+            }
+        }
+        .scaledToFit()
+    }
+}
+
+struct Badge_Previews: PreviewProvider {
+    static var previews: some View {
+        Badge()
+    }
+}
