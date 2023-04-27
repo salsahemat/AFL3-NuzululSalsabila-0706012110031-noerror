d26b2404fbc10fd1cc71e68ef1e2fb7063e54647
Nuzulul-Salsabila <nsalsabila02@student.ciputra.ac.id>
2023-04-27 21:22:59 +0700

    section 6 Creating and combining views done

--- AFL3-NuzululSalsabila-0706012110031-noerror/ContentView.swift
+++ AFL3-NuzululSalsabila-0706012110031-noerror/ContentView.swift
@@ -9,22 +9,42 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        // align the views by their leading edges
-        VStack(alignment: .leading) {
-            Text("Turtle Rock")
-                .font(.title)
-            HStack {
-                Text("Joshua Tree National Park")
-                    .font(.subheadline)
-                Spacer()
-                Text("California")
-                    .font(.subheadline)
+        VStack {
+            MapView()
+            //to extend to the top edge of the screen
+                .ignoresSafeArea(edges: .top)
+                .frame(height: 300)
+            
+            CircleImage()
+            // moving the image upwards
+                .offset(y: -130)
+                .padding(.bottom, -130)
+            
+            VStack(alignment: .leading) {
+                Text("Turtle Rock")
+                    .font(.title)
+                
+                HStack {
+                    Text("Joshua Tree National Park")
+                    Spacer()
+                    Text("California")
+                }
+                .font(.subheadline)
+                .foregroundColor(.secondary)
+                
+                Divider()
+                
+                Text("About Turtle Rock")
+                    .font(.title2)
+                Text("Descriptive text goes here.")
             }
+            .padding()
+            
+            //to push the content to the top of the screen.
+            Spacer()
         }
-        .padding()
     }
 }
-
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
